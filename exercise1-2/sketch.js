bafd9e49fff44d3920018a15b71a4690dc004e3d
ljv531-glitch <ljv531@york.ac.uk>
2025-11-26 13:30:44 +0000

    Update sketch.js

--- exercise1-2/sketch.js
+++ exercise1-2/sketch.js
@@ -2,8 +2,9 @@ let ball = {
     x: 50,
     y: 50,
     speedX: 3,
-    speedY: 3
-}
+    speedY: 3,
+    diameter: 100   
+};
 
 function setup() {
     createCanvas(600, 400);
@@ -11,13 +12,28 @@ function setup() {
 
 function draw() {
     background(255);
-    circle(ball.x, ball.y, 100);
-    if (ball.x < 50 || ball.x > width - 50) {
+
+   
+    circle(ball.x, ball.y, ball.diameter);
+
+    
+    let radius = ball.diameter / 2;
+
+    
+    if (ball.x < radius || ball.x > width - radius) {
         ball.speedX *= -1;
+        ball.diameter -= 1;   
+        console.log("Diameter:", ball.diameter);
     }
-    if (ball.y < 50 || ball.y > height - 50) {
+
+    
+    if (ball.y < radius || ball.y > height - radius) {
         ball.speedY *= -1;
+        ball.diameter -= 1;   
+        console.log("Diameter:", ball.diameter);
     }
+
+
     ball.x += ball.speedX;
     ball.y += ball.speedY;
 }
